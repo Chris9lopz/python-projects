ebeec38257c5d6efba8579c27f546c37b10b96d3
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-12-21 00:11:53 -0500

    Using the module calendar to print the month and year

---
+++ exercises/beginner/print_calendar.py
@@ -0,0 +1,8 @@
+# Exercise print a calendar for a year and specific day
+
+import calendar
+
+year = int(input('Please enter the year: '))
+month = int(input('Please enter the month: '))
+
+print(calendar.month(year, month))
