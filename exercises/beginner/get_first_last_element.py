d3a31cde0f56cb4e66ac4b0f273b68f0c4cc7448
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-12-20 22:43:14 -0500

    Create a function using positional index of the list first 0 and last one -1, then pass through the function an example list

---
+++ exercises/beginner/get_first_last_element.py
@@ -0,0 +1,15 @@
+# Exercise get the first and the last element of the example list
+
+def get_first_last(user_list):
+    first_element = user_list[0]
+    last_element = user_list[-1]
+    return first_element, last_element
+
+
+example_list = ['Java', 'JavaScript', 'Python', 'C#', 'C++']
+
+print(get_first_last(example_list))
+
+
+
+
